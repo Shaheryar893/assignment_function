5e48771c257a5bba4a65b1b5ad1761e9912e2f50
Shaheryar893 <sshehry95@gmail.com>
2024-08-30 04:55:29 -0700

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,66 @@
+
+# def calculate_age(birthdate ):
+#   today = date.today()
+#   age=today.year-birthdate.year -((today.month,today.day)< (birthdate.month.birthdate.day))
+#   return age
+# birthdate_str=input("Enter your birthdate(yyyy-mm-dd:)")
+# birthdate=date,fromisoforma (birthdate_str)
+# age=calculate_age(birthdate)
+# print(f"youare{age} years old.")
+
+# # Are of circle
+# def myfunc(rad):
+#   area=3.14*rad**2
+#   return area
+# rad= int(input("Enter radius:"))
+# print("area of circle:",myfunc(rad))
+
+# #Area of cube
+# def cube(num):
+#   return num*num*num
+# num= int(input("Enter any number:"))
+# cb=cube(num)
+# print("cube of{0} is{1}".format(num,cb))
+
+# def area_of_rec(length,width):
+#   area=length*width
+#   return area
+# length= float(input("Enter the length:"))
+# width=float(input("Enter width"))
+# print("area of rectangle is",area_of_rec(length,width))
+
+def fahrenheit_to_celsius(fahrenheit):
+  celsius= (fahrenheit -32) * 5/9
+  return round(celsius,2)
+print(fahrenheit_to_celsius(100))
+
+
+
+
+# def calcpeercentage(obtained,total):
+#     result=obtained/total*100
+#     print(result)
+# calcpeercentage(900,1000)
+
+# def time(sec,min) :
+#     outcome=sec*min
+#     print(outcome)
+# time(280,5)
+
+# height= float(input("enter height in inches:"))
+# weight= float(input("ent
+#   bmi=  weight/(height**2) *703
+# print(BMI)erweight in kg:"))
+# def BMI( hight,weight):
+   
+              
+    
+
+
+# r=float(input("Enter radius"))
+# h= float(input("Enter hright"))
+# def voliun(r,h):
+#     volium= 3.14156*r*r*h
+# print("volium of cylinder is" ,voliun)
+    
+    
